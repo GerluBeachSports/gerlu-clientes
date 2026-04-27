975eca4c3ffc4c83033f83890f8647db04027040
Breno Santos <brenoosant19@gmail.com>
2026-04-27 13:13:51 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server: {
     host: true, // ou host: '0.0.0.0'
-    port: 5174,
+    port: 5173,
   },
   plugins: [
     tailwindcss(),
